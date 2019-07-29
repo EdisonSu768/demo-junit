53ef87399901e381ddcd5ae67530ac29a9282fd9
zgsu <zgsu@alauda.io>
2019-07-29 19:19:12 +0800

    add more ignore tests

--- src/test/java/test/AccountSystemTest.java
+++ src/test/java/test/AccountSystemTest.java
@@ -15,4 +15,24 @@ public class AccountSystemTest {
     public void age2() {
         assertTrue(true);
     }
+
+    @Ignore @Test
+    public void age3() {
+        assertTrue(true);
+    }
+
+    @Ignore @Test
+    public void age4() {
+        assertTrue(true);
+    }
+
+    @Ignore @Test
+    public void age5() {
+        assertTrue(true);
+    }
+
+    @Ignore @Test
+    public void age6() {
+        assertTrue(true);
+    }
 }
