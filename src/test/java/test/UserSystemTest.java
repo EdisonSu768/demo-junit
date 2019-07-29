a2e063d81bc15e5c56ea83f1ee101d22c7c32143
zgsu <zgsu@alauda.io>
2019-07-29 17:47:09 +0800

    add more faile tests

--- src/test/java/test/UserSystemTest.java
+++ src/test/java/test/UserSystemTest.java
@@ -6,7 +6,79 @@ import static org.junit.Assert.assertTrue;
 
 public class UserSystemTest {
     @Test
-    public void name() {
+    public void name0() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name1() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name2() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name3() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name4() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name5() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name6() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name7() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name8() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name9() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name10() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name11() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name12() {
         boolean result = false;
         assertTrue(result);
     }
