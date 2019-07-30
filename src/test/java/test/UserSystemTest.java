ab7f5ca65bb02eaaded3e875f5d82c4b4cbe1c49
zgsu <zgsu@alauda.io>
2019-07-30 11:07:40 +0800

    add more faile tests

--- src/test/java/test/UserSystemTest.java
+++ src/test/java/test/UserSystemTest.java
@@ -238,4 +238,670 @@ public class UserSystemTest {
         boolean result = false;
         assertTrue(result);
     }
+
+    @Test
+    public void name39() {
+        boolean result = false;
+        assertTrue(result);
+    }
+    
+    @Test
+    public void name40() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name41() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name42() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name43() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name44() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name45() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name46() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name47() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name48() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name49() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name50() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name51() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name52() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name53() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name54() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name55() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name56() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name57() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name58() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name59() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name60() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name61() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name62() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name63() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name64() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name65() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name66() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name67() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name68() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name69() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name70() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name71() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name72() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name73() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name74() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name75() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name76() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name77() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name78() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name79() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name80() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name81() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name82() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name83() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name84() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name85() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name86() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name87() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name88() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name89() {
+        boolean result = false;
+        assertTrue(result);
+    }
+    
+    @Test
+    public void name90() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name91() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name92() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name93() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name94() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name95() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name96() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name97() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name98() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name99() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name100() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name101() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name102() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name103() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name104() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name105() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name106() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name107() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name108() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name109() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name110() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name111() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name112() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name113() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name114() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name115() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name116() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name117() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name118() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name119() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name120() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name121() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name122() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name123() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name124() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name125() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name126() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name127() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name128() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name129() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name130() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name131() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name132() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name133() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name134() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name135() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name136() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name137() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name138() {
+        boolean result = false;
+        assertTrue(result);
+    }
+    
+    @Test
+    public void name139() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name140() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name141() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name142() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name143() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name144() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name145() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name146() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name147() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name148() {
+        boolean result = false;
+        assertTrue(result);
+    }
+
+    @Test
+    public void name149() {
+        boolean result = false;
+        assertTrue(result);
+    }
 }
